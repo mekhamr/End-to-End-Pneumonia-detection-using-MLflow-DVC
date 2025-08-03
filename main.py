69610290a4458bbf4778fb74435520855203c003
mekha <mekharemanan@gmail.com>
2025-08-03 14:21:05 +0530

    Base model preparation

---
+++ main.py
@@ -0,0 +1,28 @@
+from classifier import logger
+from classifier.pipeline.stage_1_Data_ingestion import DataIngestionTrainingPipeline
+from classifier.pipeline.stage_2_preparemodel import PrepareBaseModelTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME= "Prepare Base Model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model= PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
