f5ff93bcaa5c8449ef884395ad9122b9c9341628
mekha <mekharemanan@gmail.com>
2025-08-05 10:59:35 +0530

    model_evaluation_mlflow

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from classifier import logger
 from classifier.pipeline.stage_1_Data_ingestion import DataIngestionTrainingPipeline
 from classifier.pipeline.stage_2_preparemodel import PrepareBaseModelTrainingPipeline
 from classifier.pipeline.stage_3_model_training import ModelTrainingPipeline
+from classifier.pipeline.stage_4_model_evaluation import EvaluationPipeline
+
+import dagshub
+dagshub.init(repo_owner='mekharemanan', repo_name='End-to-End-Pneumonia-detection-using-MLflow-DVC', mlflow=True)
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -30,14 +34,28 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
 STAGE_NAME = "Model Training"
 
 try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")        
-    model_trainer = ModelTrainingPipeline()
+    model_trainer = ModelTrainingPipeline()        
     model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
+
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evalution = EvaluationPipeline()
+    model_evalution.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/classifier/pipeline/stage_4_model_evaluation.py
@@ -0,0 +1,27 @@
+from classifier.config.configuration import ConfigurationManager
+from classifier.components.model_evaluation import Evaluation
+from classifier import logger
+
+STAGE_NAME= "Evaluation"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+        evaluation.log_into_mlflow()
+if __name__ == '__main__' :
+
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj =  EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
